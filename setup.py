4116cd0fac1eccb66a4aaa6f836a53ea4985e742
smilingxinyi <smilingxinyi@gmail.com>
2017-06-14 17:29:53 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,17 @@ setup(
     description = 'ANSI to UTF-8',
     url = 'https://github.com/SmilingXinyi/ansi2utf8',
     author = 'blaite',
+    author_email = 'smilingxinyi@gmail.com',
+    license = 'MIT',
     keywords = 'a2u ansi2utf8',
     packages = find_packages(exclude=['docs', 'tests*']),
     install_requires=[
         'Click',
         'ansicolors'
     ],
-    entry_points='''
-        [console_scripts]
-        a2u = ansi2utf8.main:entry
-    ''',
+    entry_points={
+        'console_scripts': [
+            'a2u = ansi2utf8.main:entry'
+        ]
+    }
 )
